fcb3d737a55968d4a1e48375c9c971e29c7220e2
Swirllyman <swirllyman@gmail.com>
2021-03-17 15:57:59 -0700

    Initial

---
+++ Assets/_MyAssets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D myBody;
+    Animator animator;
+    Vector2 movement;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        myBody.MovePosition(myBody.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
